ef6d3f995103731ee67afc60382d17c654624839
Stephen Cavender <stephen.cavender@genesys.com>
2020-10-21 13:48:57 -0400

    change app to class component instead of functional component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,35 @@ import Todos from './components/Todos'
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Todos />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting',
+        completed: false
+      }
+    ]
+  };
+
+  render() {
+    console.log(this.state.todos);
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    );
+  }
 }
 
 export default App;
