22000e74d898750602b6f1e88dc4bedd55696402
Stephen Cavender <stephen.cavender@genesys.com>
2020-10-22 15:12:07 -0400

    implement add logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/layout/Header'
 import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 
 import './App.css';
 
@@ -40,14 +41,27 @@ class App extends React.Component {
     })
   }
 
+  addTodo = title => {
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false
+    };
+
+    this.setState({ todos: [...this.state.todos, newTodo]});
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <Todos
-          todos={this.state.todos}
-          toggleComplete={this.toggleComplete}
-          deleteTodo={this.deleteTodo} />
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos
+            todos={this.state.todos}
+            toggleComplete={this.toggleComplete}
+            deleteTodo={this.deleteTodo} />
+        </div>
       </div>
     );
   }
