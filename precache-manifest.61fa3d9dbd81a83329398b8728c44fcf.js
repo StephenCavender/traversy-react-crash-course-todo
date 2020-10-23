bbf591431f5615a5ac4fc1d08a6c3e171fa22d1f
Stephen Cavender <stephen.cavender@genesys.com>
2020-10-23 10:35:24 -0400

    Updates

---
+++ precache-manifest.61fa3d9dbd81a83329398b8728c44fcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e925acbb1dddac4f94aa44b1d7c52858",
+    "url": "/traversy-react-crash-course-todo/index.html"
+  },
+  {
+    "revision": "8f969bd3f966394ace3a",
+    "url": "/traversy-react-crash-course-todo/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "91bc403d757f6e541c8f",
+    "url": "/traversy-react-crash-course-todo/static/js/2.69dd7241.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/traversy-react-crash-course-todo/static/js/2.69dd7241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f969bd3f966394ace3a",
+    "url": "/traversy-react-crash-course-todo/static/js/main.3342e563.chunk.js"
+  },
+  {
+    "revision": "a0a1608e83e1f03a4525",
+    "url": "/traversy-react-crash-course-todo/static/js/runtime-main.90383629.js"
+  }
+]);
